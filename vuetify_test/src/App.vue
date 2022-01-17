d82254a82a09bf0fd6c0653d0996c093268164b4
0101dd <aa09335665@gmail.com>
2022-01-17 13:51:37 +0800

    dashboard - 換頁

--- vuetify_test/src/App.vue
+++ vuetify_test/src/App.vue
@@ -1,16 +1,41 @@
 <template>
-  <v-app>
-    <HelloWorld />
+  <v-app id="app">
+    <v-navigation-drawer v-model="drawer" app>
+      <v-sheet color="grey lighten-4" class="pa-4">
+        <v-avatar class="mb-4" color="grey darken-1" size="64"></v-avatar>
+      </v-sheet>
+
+      <v-divider></v-divider>
+
+      <v-list rounded>
+        <v-list-item router-link to="/about">
+          <v-list-item-content>
+            <v-list-item-title>first</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item router-link to="/">
+          <v-list-item-content>
+            <v-list-item-title>second</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-main>
+      <router-view />
+    </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  data: () => ({
+    drawer: null
+    // links: [
+    //   ['mdi-inbox-arrow-down', '第一個'],
+    //   ['mdi-send', '第二個'],
+    //   ['mdi-delete', '第三個'],
+    //   ['mdi-alert-octagon', '第四個']
+    // ]
+  })
 }
 </script>
---
+++ vuetify_test/src/views/HomeView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div id="Home">
+    <h1>HOME</h1>
+  </div>
+</template>
